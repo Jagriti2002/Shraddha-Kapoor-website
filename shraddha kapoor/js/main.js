851e77daedf6b8e5d7deb8842fa4cf86ac7e0178
Jagriti Mishra <103900450+Jagriti2002@users.noreply.github.com>
2023-06-09 13:17:16 +0530

    Add files via upload

---
+++ shraddha kapoor/js/main.js
@@ -0,0 +1,57 @@
+// Header Scroll
+let nav = document.querySelector(".navbar");
+window.onscroll = function () {
+    if(document.documentElement.scrollTop > 20){
+        nav.classList.add("header-scrolled");
+    }else{
+        nav.classList.remove("header-scrolled");
+    }
+} 
+
+// nav hide
+const menu_btn = document.querySelector('.hamburger');
+const mobile_menu = document.querySelector('.mobile-nav');
+menu_btn.addEventListener('click', function () {
+  menu_btn.classList.toggle('is-active');
+  mobile_menu.classList.toggle('is-active');
+});
+
+//SCROLL REVEAL
+const revealElements = document.querySelectorAll("[data-reveal]");
+const scrollReveal = function () {
+  for (let i = 0; i < revealElements.length; i++) {
+    const elementIsInScreen = revealElements[i].getBoundingClientRect().top < window.innerHeight / 1.15;
+    if (elementIsInScreen) {
+      revealElements[i].classList.add("revealed");
+    } else {
+      revealElements[i].classList.remove("revealed");
+    }
+  }
+}
+window.addEventListener("scroll", scrollReveal);
+scrollReveal();
+
+// add Event on multiple elment
+const addEventOnElements = function (elements, eventType, callback) {
+  for (let i = 0; i < elements.length; i++) {
+    elements[i].addEventListener(eventType, callback);
+  }
+}
+
+// BOTTOM TO TOP
+const backTopBtn = document.querySelector("[data-back-top-btn]");
+window.addEventListener("scroll", function () {
+  const bodyHeight = document.body.scrollHeight;
+  const windowHeight = window.innerHeight;
+  const scrollEndPos = bodyHeight - windowHeight;
+  const totalScrollPercent = (window.scrollY / scrollEndPos) * 100;
+  backTopBtn.textContent = `${totalScrollPercent.toFixed(0)}%`;
+  // visible back top btn when scrolled 5% of the page
+  if (totalScrollPercent > 5) {
+    backTopBtn.classList.add("show");
+  } else {
+    backTopBtn.classList.remove("show");
+  }
+});
+
+
